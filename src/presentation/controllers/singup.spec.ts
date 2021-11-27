608bc2e95efe299e2739ab016661804977284425
Ian Chagas <ianchagassalgado@hotmail.com>
2021-11-27 19:04:21 -0300

    test: add test from method SingUpController

---
+++ src/presentation/controllers/singup.spec.ts
@@ -0,0 +1,17 @@
+import { SingUpController } from './singup'
+
+describe('SingUp Controller', () => {
+  test('Should return 400 if no name is provided', () => {
+    const sut = new SingUpController()
+    const httpRequest = {
+      body: {
+        email: 'ianchagas@hotmail.com',
+        password: 'password',
+        passwordConfirmation: 'password'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
