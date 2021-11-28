924c602c2a41a17941b4d5272096db1ab6af11c6
Ian Chagas <ianchagassalgado@hotmail.com>
2021-11-28 12:48:44 -0300

    refactor: move a duplicate code to a helper method

--- src/presentation/controllers/singup.spec.ts
+++ src/presentation/controllers/singup.spec.ts
@@ -1,4 +1,5 @@
 import { SingUpController } from './singup'
+import { MissingParamError } from '../errors/missing-param-error'
 
 describe('SingUp Controller', () => {
   test('Should return 400 if no name is provided', () => {
@@ -13,6 +14,20 @@ describe('SingUp Controller', () => {
 
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+    expect(httpResponse.body).toEqual(new MissingParamError('name'))
+  })
+  test('Should return 400 if no email is provided', () => {
+    const sut = new SingUpController()
+    const httpRequest = {
+      body: {
+        name: 'Ian Chagas',
+        password: 'password',
+        passwordConfirmation: 'password'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('email'))
   })
 })
