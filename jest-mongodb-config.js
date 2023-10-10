bd45c71d312280ca00c570ec714d9ce5c9a9e3e2
Ian Chagas <ianchagassalgado@hotmail.com>
2023-10-10 18:20:12 -0300

    feat: ensure AccountMongoRepository returns an account on success

--- jest-mongodb-config.js
+++ jest-mongodb-config.js
@@ -1,12 +1,10 @@
 module.exports = {
   mongodbMemoryServerOptions: {
-    instance: {
-      dbName: "jest",
-    },
     binary: {
-      version: "4.0.3",
+      version: "6.0.6",
       skipMD5: true,
     },
     autoStart: false,
+    instance: {},
   },
 };
