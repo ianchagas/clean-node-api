739e1754f8b52eca25cfa6e54e69f7c74c4b3c52
Ian <ian@meucrediario.com.br>
2024-07-31 22:03:56 -0300

    chore: remove export default files from coverage test

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   roots: ["<rootDir>/src"],
-  collectCoverageFrom: ["<rootDir>/src/**/*.ts", "!<rootDir>/src/main/*.ts"],
+  collectCoverageFrom: [
+    "<rootDir>/src/**/*.ts",
+    "!<rootDir>/src/main/*.ts",
+    "!<rootDir>/src/data/usecases/add-account/db-add-account-protocols.ts",
+    "!<rootDir>/src/presentation/controllers/singup/singup-protocols.ts",
+    "!<rootDir>/src/presentation/protocols/index.ts",
+    "!<rootDir>/src/main/config/env.ts",
+    "!<rootDir>/src/main/adapters/express-route-adapter.ts"
+  ],
   coverageDirectory: "coverage",
   testEnvironment: "node",
   preset: "@shelf/jest-mongodb",
